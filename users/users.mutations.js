b5fec82246e27c090a59364beadae26a622f1399
bnbnac <bnbnac@naver.com>
2022-07-26 10:10:06 +0900

    dividing resolvers

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import client from "../client";
+import jwt from "jsonwebtoken";
 import bcrypt, { compareSync } from "bcrypt";
 
 export default {
@@ -37,5 +38,24 @@ export default {
         return e;
       }
     },
+    login: async (_, { username, password }) => {
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "incorrect password.",
+        };
+      }
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return { ok: true, token };
+    },
+    editProfile: () => 1,
   },
 };
