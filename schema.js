db224a7eae5e6d69bd9be541c4803a7c84a9cab9
bnbnac <bnbnac@naver.com>
2022-07-27 11:17:02 +0900

    need to import 'graphql-upload'

--- schema.js
+++ schema.js
@@ -3,11 +3,7 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const typeDefs = mergeTypeDefs(loadedTypes);
-
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
-const resolvers = mergeResolvers(loadedResolvers);
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-export default schema;
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,36 +1,39 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import { protectedResolver } from "../users.utils";
 import client from "../../client";
+
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword, bio },
+  { loggedInUser }
+) => {
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      bio,
+      ...(uglyPassword && { password: uglyPassword }),
+      // spread operator
+    },
+  });
+  if (updatedUser.id) {
+    return { ok: true };
+  } else {
+    return { ok: false, error: "Could not update profile" };
+  }
+};
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword, token }
-    ) => {
-      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
-      }
-      const updatedUser = await client.user.update({
-        where: {
-          id,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassword && { password: uglyPassword }),
-          // spread operator
-        },
-      });
-      if (updatedUser.id) {
-        return { ok: true };
-      } else {
-        return { ok: false, error: "Could not update profile" };
-      }
-    },
+    editProfile: protectedResolver(resolverFn),
   },
 };
